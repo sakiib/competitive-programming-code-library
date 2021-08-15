91bda47dda212c24b2a91e5f3343e7588d0cccdb
Sakib Md Al Amin <sakibalamin162@gmail.com>
2021-08-15 20:38:42 +0600

    Update Template.cpp

--- Template/Template.cpp
+++ Template/Template.cpp
@@ -3,29 +3,32 @@ using namespace std;
 
 #define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
 template <typename Arg1>
-void __f(const char* name, Arg1&& arg1) {
-  cerr << name << " : " << arg1 << "\n";
-}
-template <typename Arg1, typename... Args>
-void __f(const char* na, Arg1&& arg1, Args&&... args) {
-  const char* c = strchr(na + 1, ',');
-  cerr.write(na, c - na) << " : " << arg1 << " , ";
-  __f(c + 1, args...);
-}
+  void __f(const char * name, Arg1 && arg1) {
+    cerr << name << " : " << arg1 << "\n";
+  }
+template <typename Arg1, typename...Args>
+  void __f(const char * na, Arg1 && arg1, Args && ...args) {
+    const char * c = strchr(na + 1, ',');
+    cerr.write(na, c - na) << " : " << arg1 << " , ";
+    __f(c + 1, args...);
+  }
 #define endl "\n"
-#define all(v) v.begin(), v.end()
-#define UNIQUE(v) sort(all(v)),v.erase(unique(all(v)),v.end())
-typedef long long LL;
-typedef pair <int,int> pii;
-const double pi = acos(-1.0);
-const double eps = 1e-8;
-const int mod = 1e9 + 7;
+#define all(V) V.begin(), V.end()
+#define Unique(V) sort(all(V)), V.erase(unique(all(V)), V.end())
+typedef long long int LL;
+typedef pair <int, int> pii;
+const int MOD = 1e9 + 7;
 const int inf = 1e9;
-const LL INF = 1e18;
+const double EPS = 1e-9;
+const double PI = acos(-1.0);
+const long long INF = 1e18;
 const int N = 1e5 + 5;
 
 int main() {
-    ios_base::sync_with_stdio(false); cin.tie(0);
-    
-    return 0;
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+#endif // LOCAL
+  ios_base::sync_with_stdio(false); cin.tie(0);
+
+  return 0;
 }
