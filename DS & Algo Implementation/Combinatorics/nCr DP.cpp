c5c90afc30e632197fc437ecdcb5a1fcc8b3d4d6
Sakib Md Al Amin <sakibalamin@appscode.com>
2021-08-31 23:19:38 +0600

    update combinatorcs

--- DS & Algo Implementation/Combinatorics/nCr DP.cpp
+++ DS & Algo Implementation/Combinatorics/nCr DP.cpp
@@ -1,7 +1,5 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-const int mod = 1000000007;
+
 long long dp[ 1005 ][ 1005 ];
 
 long long nCr( int n , int r ) {
@@ -11,11 +9,4 @@ long long nCr( int n , int r ) {
         if( dp[n][r] != -1 ) return dp[n][r];
         return dp[n][r] = nCr( n-1 , r ) + nCr( n-1 , r-1 );
 }
-int main( int argc , char const *argv[] ) {
-        int n , r;
-        memset( dp , -1 , sizeof(dp) );
-        while( cin >> n >> r ) {
-                cout << nCr( n , r ) << endl;
-        }
-        return 0;
-}
+
